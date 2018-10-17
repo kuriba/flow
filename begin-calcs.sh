48ea067e1a50f768d57b5ee617af97288fdfc8ef
Biruk Gezahegn Abreha <biruk.abreha@gmail.com>
2018-10-17 14:14:05 -0400

    fixed typo

--- begin-calcs.sh
+++ begin-calcs.sh
@@ -4,7 +4,7 @@
 # execute anywhere in workflow directory tree
 
 # source config and function files
-source-config
+source_config
 
 echo -e "\nCommencing workflow [Version $FLOW_VERSION]"
 # convert pdbs to G16 input files for PM7 optimization
@@ -13,7 +13,7 @@ total_pdbs=$(ls -f *.pdb | wc -l)
 current=1
 echo -e "\nCreating PM7 input files..."
 for file in *.pdb; do
-	progress-bar 100 0 $current $total_pdbs
+	progress_bar 100 0 $current $total_pdbs
 	bash $FLOW_TOOLS/scripts/make-com.sh -i=$file -r='#p pm7 opt' -l=$PM7 -f
 	current=$((current + 1))
 done
@@ -21,7 +21,7 @@ echo ""
 
 # submit PM7 optimization array
 cd $PM7
-PM7_ID=$(submit-array "$TITLE\_PM7" "g16_inp.txt" "com" "$FLOW_TOOLS/templates/array_g16_pm7.sbatch" $PM7_TIME)
+PM7_ID=$(submit_array "$TITLE\_PM7" "g16_inp.txt" "com" "$FLOW_TOOLS/templates/array_g16_pm7.sbatch" $PM7_TIME)
 echo "PM7 array submitted with job ID $PM7_ID"
 
 # submit RM1-D submitter which submits after PM7 array completes
