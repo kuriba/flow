ac625938a01cd845c01aef4cadf39bfe3c0ec8ba
abreha.b@husky.neu.edu <abreha.b@husky.neu.edu>
2019-05-10 10:23:06 -0400

    Created init.sh which simplifies initial setup for workflow

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script will modify the .bashrc file to add the $FLOW environment variable and 
+# source functions necessary for the workflow. The location of the .bashrc file is 
+# assumed to be ~/.bashrc; if this is not the case, specify the correct location below.
+
+bash_source=~/.bashrc
+
+# ensure .bashrc exists
+if [ ! -f "$bash_source" ]; then
+	echo "Error: .bashrc not found in $bash_source"
+	exit 1
+fi
+
+utils_header='# VERDE workflow utils'
+# do not modify .bashrc if already modified
+if [ $(grep -c "$utils_header" "$bash_source") -ge 1 ]; then
+	echo "VERDE workflow already initialized."
+	exit 0
+fi
+
+# modify .bashrc
+FLOW=$(pwd)
+echo -e "$utils_header\nexport FLOW=$FLOW\nset -a; source $FLOW/functions.sh; set +a\n" >> "$bash_source"
+echo "Successfully initialized VERDE workflow variables."
