c46c9e43b0cf89e0fed86787e8e36d79d4ed9658
abreha.b@husky.neu.edu <abreha.b@husky.neu.edu>
2019-05-11 20:46:55 -0400

    renamed variable

--- init.sh
+++ init.sh
@@ -4,22 +4,22 @@
 # source functions necessary for the workflow. The location of the .bashrc file is 
 # assumed to be ~/.bashrc; if this is not the case, specify the correct location below.
 
-bash_source=~/.bashrc
+bashrc_source=~/.bashrc
 
 # ensure .bashrc exists
-if [ ! -f "$bash_source" ]; then
-	echo "Error: .bashrc not found in $bash_source"
+if [ ! -f "$bashrc_source" ]; then
+	echo "Error: .bashrc not found at $bashrc_source"
 	exit 1
 fi
 
 utils_header='# VERDE Materials DB workflow utils'
 # do not modify .bashrc if already modified
-if [ $(grep -c "$utils_header" "$bash_source") -ge 1 ]; then
+if [ $(grep -c "$utils_header" "$bashrc_source") -ge 1 ]; then
 	echo "VERDE workflow already initialized."
 	exit 0
 fi
 
 # modify .bashrc
 FLOW=$(pwd)
-echo -e "$utils_header\nexport FLOW=$FLOW\nset -a; source $FLOW/functions.sh; set +a\n" >> "$bash_source"
+echo -e "$utils_header\nexport FLOW=$FLOW\nset -a; source $FLOW/functions.sh; set +a\n" >> "$bashrc_source"
 echo "Successfully initialized VERDE workflow variables."
