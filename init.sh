15769cc01f4c9762c22bcd5ad15b47036d7043ea
abreha.b@husky.neu.edu <abreha.b@husky.neu.edu>
2019-05-12 13:26:10 -0400

    added automatic setting of GAMESS scratch directories

--- init.sh
+++ init.sh
@@ -6,20 +6,34 @@
 
 bashrc_source=~/.bashrc
 
-# ensure .bashrc exists
+scratch_dir=/scratch/$USER
+
+utils_header='# VERDE Materials DB workflow utils'
+
+# ensure .bashrc exists and check if already modified
 if [ ! -f "$bashrc_source" ]; then
 	echo "Error: .bashrc not found at $bashrc_source"
 	exit 1
+elif [ $(grep -c "$utils_header" "$bashrc_source") -ge 1 ]; then
+	echo ".bashrc already modified."
+else
+	FLOW=$(pwd)
+	echo -e "$utils_header\nSCRATCH=$scratch_dir\nexport FLOW=$FLOW\nset -a; source $FLOW/functions.sh; set +a\n" >> "$bashrc_source"
+	echo "Successfully initialized VERDE workflow variables."
 fi
 
-utils_header='# VERDE Materials DB workflow utils'
-# do not modify .bashrc if already modified
-if [ $(grep -c "$utils_header" "$bashrc_source") -ge 1 ]; then
-	echo "VERDE workflow already initialized."
-	exit 0
+# modify rungms
+gamess_source=$(whereis rungms | awk '{print $NF}')
+
+if [ ! -d "$scratch_dir/gamess_scratch/scr" ]; then
+	mkdir -p $scratch_dir/gamess_scratch/scr
 fi
 
-# modify .bashrc
-FLOW=$(pwd)
-echo -e "$utils_header\nexport FLOW=$FLOW\nset -a; source $FLOW/functions.sh; set +a\n" >> "$bashrc_source"
-echo "Successfully initialized VERDE workflow variables."
+if [ $(grep -c '#set SCR=/scratch/$USER/gamess_scratch/' "$FLOW/utils/rungms") -eq 1 ]; then
+	sed -i 's|#set SCR=/scratch/$USER/gamess_scratch/|set SCR=$SCRATCH/gamess_scratch|' $FLOW/utils/rungms
+	sed -i 's|#set USERSCR=/scratch/$USER/gamess_scratch/scr/|set USERSCR=$SCRATCH/gamess_scratch/scr|' $FLOW/utils/rungms
+	sed -i "s|#set GMSPATH=/shared/centos7/gamess/2018R1|set GMSPATH=$gamess_source|" $FLOW/utils/rungms
+fi
+
+echo "Initialization complete."
+
