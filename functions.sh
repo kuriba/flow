571599ac623c8e9bbacfbe5f4251d6ed2cf21e9c
abreha.b@husky.neu.edu <abreha.b@husky.neu.edu>
2019-12-23 13:48:00 -0500

    simplified function

--- functions.sh
+++ functions.sh
@@ -434,7 +434,5 @@ function check_prog {
 # use: setup_flow <workflow_name>
 # effect: creates a workflow directory with the given name
 function setup_flow {
-	for d in "$@":
-		bash $FLOW/utils/setup_flow.sh $d
-	done
+	bash $FLOW/utils/setup_flow.sh "$@"
 }
